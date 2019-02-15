2d70fb7a405d3b22f53aee986daf3fd0b94810ca
Chris Hall <chall8908@gmail.com>
2019-02-15 17:58:44 -0600

    Add more documention to the Interface module

--- lib/cli/mastermind/interface.rb
+++ lib/cli/mastermind/interface.rb
@@ -1,12 +1,19 @@
+# Wraps methods from CLI::UI in a slightly nicer DSL
+# @see https://github.com/Shopify/cli-ui
 module CLI::Mastermind::Interface
+  # Enables cli-ui's STDOUT Router for fancy UIs
   def enable_ui
     CLI::UI::StdoutRouter.enable
   end
 
+  # :private:
   def ui_enabled?
     CLI::UI::StdoutRouter.enabled?
   end
 
+  # Display a spinner with a +title+ while data is being loaded
+  # @returns the value of the given block
+  # @see https://github.com/Shopify/cli-ui#spinner-groups
   def spinner(title)
     return yield unless ui_enabled?
 
@@ -30,19 +37,40 @@ module CLI::Mastermind::Interface
     yield_value
   end
 
+  # Uses +CLI::UI.fmt+ to format a string
+  # @see https://github.com/Shopify/cli-ui#symbolglyph-formatting
+  def stylize(string)
+    CLI::UI.fmt string
+  end
+
+  # Opens a CLI::UI frame with the given +title+
+  # @see https://github.com/Shopify/cli-ui#nested-framing
   def frame(title)
     return yield unless ui_enabled?
     CLI::UI::Frame.open(title) { yield }
   end
 
+  # Ask the user for some text.
+  # @see https://github.com/Shopify/cli-ui#free-form-text-prompts
   def ask(question, default: nil)
     CLI::UI.ask(question, default: default)
   end
 
+  # Ask the user a yes/no +question+
+  # @see https://github.com/Shopify/cli-ui#interactive-prompts
   def confirm(question)
     CLI::UI.confirm(question)
   end
 
+  # Display an interactive list of options for the user to select.
+  #
+  # @param +question+ The question to ask the user
+  # @param +options:+ Array|Hash the options to display
+  # @param +default:+ The value or key to display first.  Defaults to the first option.
+  #
+  # Any other keyword arguments given are passed down into +CLI::UI::Prompt.ask+.
+  #
+  # @see https://github.com/Shopify/cli-ui#interactive-prompts
   def select(question, options:, default: options.first, **opts)
     default_value = nil
     options = case options
@@ -68,6 +96,15 @@ module CLI::Mastermind::Interface
     end
   end
 
+  # Titleize the given +string+.
+  # Replaces any dashes (-) or underscores (_) in the +string+ with spaces and
+  # then capitalizes each word.
+  #
+  # Examples:
+  #   titleize('foo') => 'Foo'
+  #   titleize('foo bar') => 'Foo Bar'
+  #   titleize('foo-bar') => 'Foo Bar'
+  #   titleize('foo_bar') => 'Foo Bar'
   def titleize(string)
     string.gsub(/[-_-]/, ' ').split(' ').map(&:capitalize).join(' ')
   end
