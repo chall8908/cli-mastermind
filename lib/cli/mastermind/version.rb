de4f158d600280f470205412743a695195da3c72
Chris Hall <chall8908@gmail.com>
2019-02-28 17:19:13 -0600

    Bump version to 0.2.1

--- lib/cli/mastermind/version.rb
+++ lib/cli/mastermind/version.rb
@@ -8,7 +8,7 @@ module CLI
     module VERSION
       RELEASE = 0
       MAJOR = 2
-      MINOR = 0
+      MINOR = 1
       PATCH = nil
 
       STRING = [RELEASE, MAJOR, MINOR, PATCH].compact.join('.').freeze
